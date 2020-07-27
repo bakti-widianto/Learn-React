888adfa0f92a6006fb30e4dace0b0986fa1e386e
bakti <bakti.widianto@gmail.com>
2020-07-27 23:07:53 +0700

    try get and post to api

--- todo/src/components/BoxItem.js
+++ todo/src/components/BoxItem.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
 import FormItem from './FormItem'
 import ListItem from './ListItem'
+import axios from 'axios'
+
+const request = axios.create({
+    baseURL: 'http://localhost:3001/api/',
+    timeout: 1000,
+    headers: { 'token': 'adnosnacacd3144342@$' }
+});
 
 
 export default class BoxItem extends Component {
     constructor(props) {
         super(props);
-        this.state = { todos: ["masak", "belajar"] }
+        this.state = { todos: [] }
 
         this.addTodo = this.addTodo.bind(this);
+    }
+
+    componentDidMount() {
+        request.get('/todos')
+            .then(function (response) {
+                // console.log(response)
+                this.setState({ todos: response.data });
+            }.bind(this))
+            .catch(function (error) {
+                alert(error);
+            })
     }
 
     addTodo(todo) {
         this.setState((state, props) => ({
             todos: [...state.todos, todo]
         }));
+        request.post('/todos',{
+            task: todo
+        })
+            .then(function (response) {
+                // console.log(response)
+                this.setState({ todos: response.data });
+            }.bind(this))
+            .catch(function (error) {
+                alert(error);
+            })
     }
 
 
--- todo/src/components/Item.js
+++ todo/src/components/Item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Item(props) {
-    return <li>{props.no} {props.task}</li>;
+    return <li>{props.no}. {props.task}</li>;
 }
 
 export default Item;
